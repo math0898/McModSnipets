be44789e925408202de90951e5c7c1f727dee37b
math0898@users.noreply.github.com <math0898@users.noreply.github.com>
2021-09-19 18:31:55 -0500

    v1.4.1-release1

--- src/main/java/io/github/math0898/LocationHelper.java
+++ src/main/java/io/github/math0898/LocationHelper.java
@@ -21,4 +21,20 @@ public class LocationHelper {
     public static Location deltaLocation (Location location, double dx, double dy, double dz) {
         return new Location(location.getWorld(), location.getX() + dx, location.getY() + dy, location.getZ() + dz);
     }
+
+    /**
+     * Determines the equality of locations. Returning true if and only if the locations match.
+     *
+     * @param l1 The first location.
+     * @param l2 The second location.
+     * @param delta The difference each distance coordinate must be within to be considered this location.
+     * @return True if and only if the locations match.
+     */
+    private boolean compare (Location l1, Location l2, double delta) {
+        if (l1.getWorld() == null || l2.getWorld() == null) return false;
+        if (!(Math.abs(l1.getX() - l2.getX()) < delta)) return false;
+        if (!(Math.abs(l1.getY() - l2.getY()) < delta)) return false;
+        if (!(Math.abs(l1.getZ() - l2.getZ()) < delta)) return false;
+        return l1.getWorld().getName().equals(l2.getWorld().getName());
+    }
 }
